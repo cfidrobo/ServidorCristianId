a06816134701d6dbe12788d0e2eb37c894999235
cfidrobo <105193365+cfidrobo@users.noreply.github.com>
2022-05-27 16:19:02 -0500

    Update servidorweb.js

--- servidorweb.js
+++ servidorweb.js
@@ -1,13 +1,11 @@
-const http =require('http');
-
-const port =3000;
-
-const server= http.createServer((req, res) =>{
-  res.statusCode =200;
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hola Mundo');
- });
- 
- server.listen(port, () => {
+});
+
+server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
- });
+});
